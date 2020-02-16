6aa24afa3029c5b02b48d6d1c509e59714fea63c
栗子好好吃 <543288744@qq.com>
2020-02-16 12:44:32 +0800

    day02

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,36 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Marron',
+    age: 18,
+    students: [{
+        id: 110,
+        name: 'ko',
+        age: 30
+      },
+      {
+        id: 9527,
+        name: 'chou',
+        age: 18
+      },
+      {
+        id: 111,
+        name: 'curry',
+        age: 33
+      }
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 1. 错误做法: 界面不会刷新
+    // this.data.counter +=1;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnSubstract() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
